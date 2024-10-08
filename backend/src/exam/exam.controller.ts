0ff0d45079db95d35d5c8a1c40ea336291c2aef5
Awalhossain <waqin21@gmail.com>
2024-10-08 19:37:29 +0600

    refactor: Simplify ExamController in exam.controller.ts

--- backend/src/exam/exam.controller.ts
+++ backend/src/exam/exam.controller.ts
@@ -1,29 +1,13 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { CreateExamDto, CreateExamSessionDto } from './dtos/createExam.dto';
-import { ExamService } from './exam.service';
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateExamDto } from "./dtos/createExam.dto";
+import { ExamService } from "./exam.service";
 
-@Controller('exam')
+@Controller("exam")
 export class ExamController {
   constructor(private examService: ExamService) {}
 
-  @Post('/create')
-  @UsePipes(ValidationPipe)
+  @Post("/create")
   createExam(@Body() data: CreateExamDto) {
     return this.examService.createExam(data);
-  }
-
-  @Get('/get-all')
-  getAllExams() {
-    return this.examService.getAllExams();
-  }
-
-  @Post('/session')
-  createExamSession(@Body() data: CreateExamSessionDto) {
-    return this.examService.startExamSession(data);
-  }
-
-  @Post('/start')
-  startExamSession(@Body() data: CreateExamSessionDto) {
-    return this.examService.startExam(data);
   }
 }
