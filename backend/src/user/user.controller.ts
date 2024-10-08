0c770f050c4505529ca3b3764ec36929be3d0598
Awalhossain <waqin21@gmail.com>
2024-10-08 19:34:44 +0600

    refactor: Rename user controller route to '/users' and add 'getAllUsers' endpoint

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UsersService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private usersService: UsersService) {}
 
   @Post('/create')
   createUser(@Body() data: CreateUserDto) {
     return this.usersService.createUser(data);
+  }
+
+  @Get('/all')
+  getAllUsers() {
+    return this.usersService.getAllUsers();
   }
 }
