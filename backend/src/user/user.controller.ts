97cc32a82a291c8ac20099e98b7f812796b6114e
Awalhossain <waqin21@gmail.com>
2024-10-10 11:42:18 +0600

    refactor: Update import statements in user.controller.ts

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,17 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/createUser.dto';
-import { UsersService } from './user.service';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dtos/createUser.dto";
+import { UsersService } from "./user.service";
 
-@Controller('users')
+@Controller("users")
 export class UserController {
   constructor(private usersService: UsersService) {}
 
-  @Post('/create')
+  @Post("/create")
   createUser(@Body() data: CreateUserDto) {
     return this.usersService.createUser(data);
   }
 
-  @Get('/all')
+  @Get("/all")
   getAllUsers() {
     return this.usersService.getAllUsers();
   }
