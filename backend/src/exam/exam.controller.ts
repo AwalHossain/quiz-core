ddd1f95a9565409489127575bd7a591d96552358
Awalhossain <waqin21@gmail.com>
2024-10-10 11:41:29 +0600

    refactor: Update exam.controller.ts with additional endpoints and validation pipes

--- backend/src/exam/exam.controller.ts
+++ backend/src/exam/exam.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { CreateExamDto } from "./dtos/createExam.dto";
+import { Body, Controller, Get, Post, Query, UsePipes, ValidationPipe } from "@nestjs/common";
+import { CreateExamDto, CreateExamSessionDto, CreateSubmissionDto } from "./dtos/createExam.dto";
 import { ExamService } from "./exam.service";
 
 @Controller("exam")
@@ -9,5 +9,32 @@ export class ExamController {
   @Post("/create")
   createExam(@Body() data: CreateExamDto) {
     return this.examService.createExam(data);
+  }
+
+  @Get("/all")
+  getAllExams() {
+    return this.examService.getAllExams();
+  }
+
+  @Post("/session")
+  @UsePipes(ValidationPipe)
+  createExamSession(@Body() data: CreateExamSessionDto) {
+    return this.examService.startOrResumeExam(data.userId, data.examId);
+  }
+
+  @Post("/submit")
+  @UsePipes(ValidationPipe)
+  submitQuestionAnswer(@Body() data: CreateSubmissionDto) {
+    return this.examService.submitQuestionAnswer(data);
+  }
+
+  @Get("/current")
+  getCurrentQuestion(
+    @Query("examSessionId") examSessionId: number,
+    @Query("action") action: "next" | "previous" | "current"
+  ) {
+    console.log(examSessionId, action, "examSessionId, action");
+
+    return this.examService.getCurrentQuestion(Number(examSessionId), action);
   }
 }
