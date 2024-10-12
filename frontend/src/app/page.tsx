ef1c9a56b0032b25f16652da29bbbd9b52d97611
Awalhossain <waqin21@gmail.com>
2024-10-12 23:27:26 +0600

    refactor: Remove unused code from layout.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,8 @@
-import ExamCards from "../components/card/ExamCards";
+import { redirect } from "next/navigation";
 
 
+// redirect to group page
 export default function Home() {
-  return (
-    <div className="space-x-1 p-5 pt-0">
-      {/* <Hero /> */}
-      <ExamCards />
-      {/* <Button className="bg-primary">Primary</Button>
-      <Button className="bg-secondary">আরো দেখুন</Button>
-      <Button className="bg-custom-red"> দেখুন</Button>
-      <Button className="bg-custom-gray-strong"> Gray Strong</Button>
-      <Button className="bg-custom-content-secondary">Content Secondary</Button>
-      <p className="my-5 text-black dark:text-white">আমার সোনার বাংলা</p> */}
-      {/* <ModeToggle /> */}
-    </div>
-  );
+  //  redirect group page component
+  return redirect("/home");
 }
