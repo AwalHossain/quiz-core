4bfd20fb9e73e297114e1134712285c3c59e741a
Awalhossain <waqin21@gmail.com>
2024-10-12 23:28:46 +0600

    feat: Add ExamDetailsPage component

---
+++ frontend/src/app/exam/[examId]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+const ExamDetailsPage = ({ params }: { params: { examId: string } }) => {
+    console.log(params, "params");
+    return (
+        <div>
+            {/* MCQ  */}
+
+        </div>
+    )
+}
+
+export default ExamDetailsPage;
