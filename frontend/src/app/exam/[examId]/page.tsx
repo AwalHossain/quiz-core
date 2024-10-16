2d978c281b9a7e22cc4dfb2b5c25f139915ca87c
Awalhossain <waqin21@gmail.com>
2024-10-16 12:36:34 +0600

    refactor exam session page to start or resume exam session, show exam timer, and display exam questions

--- frontend/src/app/exam/[examId]/page.tsx
+++ frontend/src/app/exam/[examId]/page.tsx
@@ -1,14 +1,45 @@
-"use client";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**
+ * this page will start or resume the exam session
+ * and will show the exam questions
+ * @param param0 
+ * @returns 
+ */
 
 
-const ExamDetailsPage = ({ params }: { params: { examId: string } }) => {
-    console.log(params, "params");
+import { redirect } from "next/navigation";
+import { ExamSession } from "../../../services/session";
+import ExamTimer from "./features/ExamTimer";
+import MCQQuiz from "./features/MCQQuiz";
+
+const ExamSessionPage = async ({ params }: { params: { examId: string } }) => {
+    const { data, status, statusText } = await ExamSession(parseInt(params.examId), 4);
+    console.log(data, "session", status);
+
+    if (data.status === "FINISHED") {
+        // redirect to result page
+
+
+        return (
+            `Exam Finished, redirecting to leaderboard ${redirect(`/leaderboard`)}`
+        );
+
+    }
+
+
     return (
         <div>
-            {/* MCQ  */}
 
+            {/* Show exam timer here */}
+            <ExamTimer initialTime={data.remainingTime} duration={data.duration} examId={parseInt(params.examId)} />
+            {/* Show exam questions here */}
+            <div>
+                <h1>Exam Questions</h1>
+            </div>
+            <MCQQuiz />
         </div>
     )
 }
 
-export default ExamDetailsPage;
+export default ExamSessionPage;
