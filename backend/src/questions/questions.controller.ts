b54967bc65e24fb46eb982d4b00dc6c2f6f3aaf5
Awalhossain <waqin21@gmail.com>
2024-10-16 12:30:23 +0600

    refactor questions.controller.ts to add new endpoints and improve code readability

--- backend/src/questions/questions.controller.ts
+++ backend/src/questions/questions.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from "@nestjs/common";
 import { CreateQuestionDto } from "./dtos/createQuestio.dto";
 import { QuestionsService } from "./questions.service";
 
@@ -15,5 +24,20 @@ export class QuestionsController {
   @Get("/all")
   getAllQuestions() {
     return this.questionsService.getAllQuestions();
+  }
+
+  @Get("/:examId")
+  getQuestionsByExamId(@Param("examId") examId: number) {
+    return this.questionsService.getQuestionsByExamId(examId);
+  }
+
+  @Get("/current")
+  getCurrentQuestion(
+    @Query("examSessionId") examSessionId: number,
+    @Query("action") action: "next" | "previous" | "current"
+  ) {
+    console.log(examSessionId, action, "examSessionId, action");
+
+    return this.questionsService.getCurrentQuestion(Number(examSessionId), action);
   }
 }
