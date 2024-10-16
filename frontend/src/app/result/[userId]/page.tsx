8a22cdc117bbf66f63de24b93f7e9dccc29414a0
Awalhossain <waqin21@gmail.com>
2024-10-16 12:38:39 +0600

    Add ResultPage component

---
+++ frontend/src/app/result/[userId]/page.tsx
@@ -0,0 +1,9 @@
+
+const ResultPage = ({ params }: { params: { userId: string } }) => {
+    console.log(params.userId, "userId");
+    return (
+        <div>ResultPage</div>
+    )
+}
+
+export default ResultPage
