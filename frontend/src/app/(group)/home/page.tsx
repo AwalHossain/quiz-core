1311078194f2a18ce6a7d0d37a90252e83018114
Awalhossain <waqin21@gmail.com>
2024-10-16 12:34:21 +0600

    refactor: Update ExamCards component to accept dynamic data

--- frontend/src/app/(group)/home/page.tsx
+++ frontend/src/app/(group)/home/page.tsx
@@ -1,12 +1,12 @@
-import { getAllExam } from "@/services/session";
 import ExamCards from "../../../components/card/ExamCards";
+import { getAllExam } from "../../../services/session";
 
 const HomePage = async () => {
     const getQuestions = await getAllExam();
     console.log(getQuestions.data, "getQuestions");
     return (
         <div>
-            <ExamCards />
+            <ExamCards questions={getQuestions.data} />
         </div>
     )
 }
