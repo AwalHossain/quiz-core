0bc7c9b3b4b59df1e316de2b5c33a5a2ce4f73e4
Awalhossain <waqin21@gmail.com>
2024-10-16 12:32:10 +0600

    refactor: Update Exam interface in constants.d.ts

--- frontend/src/services/session/index.ts
+++ frontend/src/services/session/index.ts
@@ -1,18 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AxiosResponse } from "axios";
 import { clientFetch } from "../client-fetch";
 import { ENDPOINTS } from "../endpoints";
 
-type Exam = {
-  id: number;
-  title: string;
-  description: string;
-  duration: number;
-}
+
 
 export const getAllExam = 
     async (): Promise<AxiosResponse<Exam[]>> => {
       try {
-        const response = await clientFetch.get(ENDPOINTS['GETALLEXAM']);
+        const response = await clientFetch.get(ENDPOINTS['GETALL_EXAM']);
         if (response.data) {
           return response;
         } else {
@@ -22,4 +18,22 @@ export const getAllExam =
         console.error('Error fetching user data: ', error);
         return Promise.reject(error);
       }
+    }
+
+
+    export const ExamSession = async (examId: number, userId: number): Promise<AxiosResponse<any>> => {
+        try {
+            const response = await clientFetch.post(ENDPOINTS['EXAM_SESSION'], {
+                examId,
+                userId
+            });
+            if (response.data) {
+                return response;
+            } else {
+                return Promise.reject(response);
+            }
+        } catch (error: any) {
+            console.error('Error starting or resuming exam session: ', error.response.data);
+            return Promise.reject(error);
+        }
     }
