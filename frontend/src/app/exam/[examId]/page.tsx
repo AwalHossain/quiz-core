4c5695df8c29c421f39a258e4aed5dc3e65628cc
Awalhossain <waqin21@gmail.com>
2024-10-21 16:17:48 +0600

    refactor: Update ExamSessionPage to include ExamProvider and ExamContent components

--- frontend/src/app/exam/[examId]/page.tsx
+++ frontend/src/app/exam/[examId]/page.tsx
@@ -1,45 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/**
- * this page will start or resume the exam session
- * and will show the exam questions
- * @param param0 
- * @returns 
- */
-
-
-import { redirect } from "next/navigation";
+// ExamSessionPage.tsx
+import { ExamProvider } from "@/context/ExamProvider";
+import { GetCurrentQuestion } from "@/services/questions";
 import { ExamSession } from "../../../services/session";
+import ExamContent from "./features/ExamContent";
 import ExamTimer from "./features/ExamTimer";
-import MCQQuiz from "./features/MCQQuiz";
 
 const ExamSessionPage = async ({ params }: { params: { examId: string } }) => {
-    const { data, status, statusText } = await ExamSession(parseInt(params.examId), 4);
-    console.log(data, "session", status);
-
-    if (data.status === "FINISHED") {
-        // redirect to result page
-
-
-        return (
-            `Exam Finished, redirecting to leaderboard ${redirect(`/leaderboard`)}`
-        );
-
-    }
-
+    const { data } = await ExamSession(params.examId, "7a48fb62-4321-4b6c-82a8-35ecdaadcc08");
+    const { data: currentQuestion } = await GetCurrentQuestion(data.id, "current");
 
     return (
         <div>
-
-            {/* Show exam timer here */}
-            <ExamTimer initialTime={data.remainingTime} duration={data.duration} examId={parseInt(params.examId)} />
-            {/* Show exam questions here */}
+            <h1>Exam Questions</h1>
             <div>
-                <h1>Exam Questions</h1>
+                <ExamTimer initialTime={data.remainingTime} duration={data.duration} examId={parseInt(params.examId)} />
+
             </div>
-            <MCQQuiz />
+            {currentQuestion && (
+                <ExamProvider initialExamState={currentQuestion}>
+                    <ExamContent examSessionId={data.id} />
+                </ExamProvider>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default ExamSessionPage;
+
+
