699131b319d637fd06748881904acf0987b7031d
Awalhossain <waqin21@gmail.com>
2024-10-21 16:19:21 +0600

    feat: Add Question and ExamContextType interfaces

--- frontend/src/types/constants.d.ts
+++ frontend/src/types/constants.d.ts
@@ -1,15 +1,41 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+interface QuestionOption {
+  id: string;
+  optionText: string;
+  optionLetter: string;
+  questionId: string;
+}
+
+export interface Question {
+  id: string;
+  examId: string;
+  questionText: string;
+  correctOptionId: string;
+  type: string;
+  questionOption: QuestionOption[];
+}
+
+export interface ExamContextType {
+  currentQuestion: Question;
+  setCurrentQuestion: (question: Question) => void;
+  currentIndex: number;
+  setCurrentIndex: (index: number) => void;
+  isLast: boolean;
+  setIsLast: (isLast: boolean) => void;
+  totalQuestions: number;
+  setTotalQuestions: (total: number) => void;
+  submission: any | null; // Replace 'any' with a more specific type if possible
+  setSubmission?: (submission: any | null) => void;
+}
 
 
-interface Exam {
-  id: number;
-  title: string;
-  description: string;
-  duration: number;
-  startTime: Date | null;
-  endTime: Date | null;
-  status: string;
-  passingScore: number | null;
-  questionCount: number;
-  icon: any;
+export interface ExamProviderProps {
+  children: ReactNode;
+  initialExamState: {
+    question: Question;
+    currentIndex: number;
+    isLast: boolean;
+    totalQuestions: number;
+    submission: any | null;
+  };
 }
