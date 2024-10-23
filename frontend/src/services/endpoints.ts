8d3d9592862e4657bd177e7f06787249658cfa18
Awalhossain <waqin21@gmail.com>
2024-10-23 21:54:25 +0600

    refactor: update endpoints in endpoints.ts

--- frontend/src/services/endpoints.ts
+++ frontend/src/services/endpoints.ts
@@ -2,8 +2,10 @@
 export const ENDPOINTS = {
   LOGIN: '/auth/login',
   GETALL_EXAM: '/exam/all',
-  EXAM_SESSION: '/exam/session',
+  EXAM_SESSION: 'exam/session',
   EXAM_QUESTIONS: '/questions',
   FINISH: '/exam/finish',
   SUBMIT: '/exam/submit',
+  RESULT: '/exam/result',
+  LEADERBOARD: '/exam',
 };
