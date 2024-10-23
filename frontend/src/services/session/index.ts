85867a0b50b6d557c32eecfa4144fe584d547672
Awalhossain <waqin21@gmail.com>
2024-10-23 21:51:09 +0600

    refactor: adjust getAllExam function to accept userId parameter

--- frontend/src/services/session/index.ts
+++ frontend/src/services/session/index.ts
@@ -5,10 +5,14 @@ import { ENDPOINTS } from "../endpoints";
 
 
 
-export const getAllExam = 
-    async (): Promise<AxiosResponse<Exam[]>> => {
-      try {
-        const response = await clientFetch.get(ENDPOINTS['GETALL_EXAM']);
+export const getAllExam =
+    async ({ userId }: { userId?: string}): Promise<AxiosResponse<Exam[]>> => {
+        try {
+            const response = await clientFetch.get(ENDPOINTS['GETALL_EXAM'], {
+                params: {
+                    userId
+                }
+            });
         if (response.data) {
           return response;
         } else {
@@ -21,19 +25,17 @@ export const getAllExam =
     }
 
 
-    export const ExamSession = async (examId: number, userId: number): Promise<AxiosResponse<any>> => {
-        try {
-            const response = await clientFetch.post(ENDPOINTS['EXAM_SESSION'], {
-                examId,
-                userId
-            });
-            if (response.data) {
+export const ExamSession = async (sessionId: string, userId: string): Promise<AxiosResponse<any>> => {
+    try {
+        const url = `${ENDPOINTS['EXAM_SESSION']}/${sessionId}/${userId}`;
+        const response = await clientFetch.get(url);
+        if (response.data) {
                 return response;
             } else {
                 return Promise.reject(response);
             }
         } catch (error: any) {
-            console.error('Error starting or resuming exam session: ', error.response.data);
-            return Promise.reject(error);
-        }
+        console.error('Error starting or resuming exam session: ', error.response.data);
+        return Promise.reject(error);
     }
+}
