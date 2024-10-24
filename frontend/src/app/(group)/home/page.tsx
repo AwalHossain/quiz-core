00d28d88cf30ca33ab754e9720dde2b37472dd41
Awalhossain <waqin21@gmail.com>
2024-10-24 10:37:58 +0600

    refactor: Update getAllExam function to include userId parameter

--- frontend/src/app/(group)/home/page.tsx
+++ frontend/src/app/(group)/home/page.tsx
@@ -2,7 +2,7 @@ import ExamCards from "../../../components/card/ExamCards";
 import { getAllExam } from "../../../services/session";
 
 const HomePage = async () => {
-    const getQuestions = await getAllExam();
+    const getQuestions = await getAllExam({ userId: "5d042e01-f949-4c58-89a8-1c5e1d7b953b" });
     console.log(getQuestions.data, "getQuestions");
     return (
         <div>
