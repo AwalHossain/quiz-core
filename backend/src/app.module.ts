d34b155b630074c11072c7c19b530bc172135d92
Awalhossain <waqin21@gmail.com>
2024-10-24 10:40:46 +0600

    feat: Add AuthModule to app imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { ExamModule } from "./exam/exam.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { QuestionsModule } from "./questions/questions.module";
 import { UserModule } from "./user/users.module";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ExamModule, QuestionsModule],
+  imports: [PrismaModule, UserModule, ExamModule, QuestionsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
