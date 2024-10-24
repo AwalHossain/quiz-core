b8577aca6ed6167e4bedf6739e12d5c47b1d8f8f
Awalhossain <waqin21@gmail.com>
2024-10-24 10:38:21 +0600

    refactor: Remove unused components and files related to exam session

--- frontend/src/app/result/[userId]/page.tsx
+++
@@ -1,9 +0,0 @@
-
-const ResultPage = ({ params }: { params: { userId: string } }) => {
-    console.log(params.userId, "userId");
-    return (
-        <div>ResultPage</div>
-    )
-}
-
-export default ResultPage
