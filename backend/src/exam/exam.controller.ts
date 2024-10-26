d6f3f0c82fd158c29128889f8acd70d446cfa43c
Awalhossain <waqin21@gmail.com>
2024-10-26 14:42:59 +0600

    refactor: update ExamController to handle public routes and extract token from header

--- backend/src/exam/exam.controller.ts
+++ backend/src/exam/exam.controller.ts
@@ -6,9 +6,12 @@ import {
   Param,
   Post,
   Query,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from "@nestjs/common";
+import { SkipAuth } from "../auth/decorators/public.decorators";
+import { AuthGuard } from "../auth/guards/auth.guard";
 import { CreateExamDto, CreateSubmissionDto } from "./dtos/createExam.dto";
 import { ExamService } from "./exam.service";
 
@@ -16,11 +19,13 @@ import { ExamService } from "./exam.service";
 export class ExamController {
   constructor(private examService: ExamService) {}
 
+  @UseGuards(AuthGuard)
   @Post("/create")
   createExam(@Body() data: CreateExamDto) {
     return this.examService.createExam(data);
   }
 
+  @SkipAuth()
   @Get("/all")
   getAllExams(@Query("userId") userId?: string) {
     console.log(userId, "userId");
@@ -28,6 +33,7 @@ export class ExamController {
     return this.examService.getAllExams(userId);
   }
 
+  @UseGuards(AuthGuard)
   @Get("/session/:sessionId/:userId")
   @UsePipes(ValidationPipe)
   async createExamSession(@Param("sessionId") sessionId: string, @Param("userId") userId: string) {
@@ -38,23 +44,26 @@ export class ExamController {
     }
   }
 
+  @UseGuards(AuthGuard)
   @Post("/submit")
   @UsePipes(ValidationPipe)
   submitQuestionAnswer(@Body() data: CreateSubmissionDto) {
     return this.examService.submitQuestionAnswer(data);
   }
 
+  @UseGuards(AuthGuard)
   @Post("/finish")
   submitOrFinishExam(@Body() data: { examSessionId: string }) {
     return this.examService.submitOrFinishExam(data.examSessionId);
   }
-
+  @UseGuards(AuthGuard)
   @Get("/result")
   getExamResult(@Query("userId") userId: string, @Query("examId") examId: string) {
     console.log(userId, examId, "userId, examId");
     return this.examService.getExamResultByExamId(userId, examId);
   }
 
+  @UseGuards(AuthGuard)
   @Get("/:examId/leaderboard")
   getExamLeaderboard(@Param("examId") examId: string, @Query("limit") limit: number = 10) {
     console.log(examId, limit, "examId, limit");
