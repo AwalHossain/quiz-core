8a4b38002430aab6748cb902c018b9c68442e679
Awalhossain <waqin21@gmail.com>
2024-10-26 14:34:32 +0600

    refactor: update AuthService to include sign up and sign in functionality

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from "@nestjs/common";
+
+import { UsersService } from "../user/user.service";
+import { JwtTokenService } from "./jwt.service";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtTokenService: JwtTokenService
+  ) {}
+
+  async signUp(username: string, password: string) {
+    const user = await this.usersService.createUser({ username, password });
+    if (!user) throw new HttpException("User not found", 404);
+    // if (user.password !== password) throw new HttpException("Invalid password", 401);
+    const payload = { sub: user.id, username: user.username };
+    console.log(payload, "payload", user);
+
+    return {
+      access_token: await this.jwtTokenService.generateToken(payload),
+    };
+  }
+
+  async signIn(username: string, password: string) {
+    const user = await this.usersService.findUserByUsername(username, password);
+    if (!user) throw new HttpException("User not found", 404);
+    const payload = { sub: user.id, username: user.username };
+    return {
+      access_token: await this.jwtTokenService.generateToken(payload),
+    };
+  }
+}
