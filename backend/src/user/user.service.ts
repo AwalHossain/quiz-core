938ca73fd18099dcf29c257a9e2d601e67aa53c5
Awalhossain <waqin21@gmail.com>
2024-10-26 14:52:55 +0600

    refactor: Update UsersService createUser method to handle existing usernames and select specific fields

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -7,14 +7,33 @@ export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: Prisma.UserCreateInput) {
-    const user = await this.prisma.user.findUnique({
+    const existingUser = await this.prisma.user.findUnique({
       where: { username: data.username },
     });
-    if (user) throw new HttpException("User not found", 404);
+    if (existingUser) throw new HttpException("Username already exists", 400);
     return this.prisma.user.create({
-      data,
-      include: { examSession: true, result: true },
+      data: {
+        username: data.username,
+        password: data.password,
+      },
+      select: { id: true, username: true, password: true },
     });
+  }
+
+  async findUserByUsername(username: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { username },
+      select: { id: true, username: true, password: true },
+    });
+    console.log(user, password, "user");
+
+    if (!user) throw new HttpException("User not found", 404);
+    // check if password is correct
+    if (user.password !== password) throw new HttpException("Invalid password", 401);
+    return {
+      id: user.id,
+      username: user.username,
+    };
   }
 
   async getAllUsers() {
