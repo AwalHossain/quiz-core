07d1ab9bc6a306e4c80b1b3a9ef6898cbee8dc14
Awalhossain <waqin21@gmail.com>
2024-10-26 14:32:51 +0600

    refactor: update app.module.ts imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
 import { ExamModule } from "./exam/exam.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { QuestionsModule } from "./questions/questions.module";
 import { UserModule } from "./user/users.module";
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [PrismaModule, UserModule, ExamModule, QuestionsModule, AuthModule],
