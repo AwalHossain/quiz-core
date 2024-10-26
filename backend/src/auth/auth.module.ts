d9b6d2ce3fb4c91699b933f5c0530898becb3d09
Awalhossain <waqin21@gmail.com>
2024-10-26 14:35:15 +0600

    refactor: update authModule imports and providers

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConstants } from "../common/constant";
+import { UserModule } from "../user/users.module";
 import { AuthController } from "./auth.controller";
-import { AuthService } from './auth.service';
+import { AuthService } from "./auth.service";
+import { JwtTokenService } from "./jwt.service";
 
 @Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtTokenService],
+  exports: [AuthService, JwtTokenService],
 })
 export class AuthModule {}
