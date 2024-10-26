f6569411e21571302111b982920c1ee32123822e
Awalhossain <waqin21@gmail.com>
2024-10-26 14:55:12 +0600

    refactor: Update LoginForm component to include login functionality and improve UI

--- frontend/src/app/(group)/home/auth/LoginForm.tsx
+++ frontend/src/app/(group)/home/auth/LoginForm.tsx
@@ -1,56 +1,97 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import FacebookIcon from '@/assets/icons/Facebook.svg';
-import Image from 'next/image';
-import { z } from 'zod';
+import { setAuthToken } from '@/action/set-cookie';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { getAxiosError } from '@/lib/getAxiosError';
+import { SignIn } from '@/services/auth';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
-const loginSchema = z.object({
-    phone: z.string().length(11, "Phone number must be 11 digits")
-})
 
 interface LoginFormProps {
-    onForgotPassword: () => void
+    onRegistration: () => void
+    onClose: () => void
 }
 
-const LoginForm: React.FC<LoginFormProps> = ({ onForgotPassword }) => {
-    // console.log(dictionary.registration, "dictionary");
+const LoginForm: React.FC<LoginFormProps> = ({ onRegistration, onClose }) => {
+    const router = useRouter();
 
-
-
-    const handleSubmit = (data: z.infer<typeof loginSchema>) => {
-        console.log(data, "data from login form");
-        onForgotPassword()
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const username = formData.get('username') as string;
+        const password = formData.get('password') as string;
+        console.log(username, "data from login form");
+        try {
+            const response = await SignIn(username, password);
+            if (response && response.access_token) {
+                await setAuthToken('access_token', response.access_token);
+                console.log(response.access_token, "response from login form");
+                toast.success('লগইন সফলভাবে সম্পন্ন হয়েছে!');
+                window.location.href = "/home";
+            } else {
+                throw new Error('Invalid response from server');
+            }
+        } catch (error: any) {
+            console.error('Error starting or resuming exam session: ', error.response);
+            if (error instanceof AxiosError) {
+                const err = getAxiosError(error);
+                toast.error(`${err.message}, ${err.status}`);
+            }
+        }
+        onClose();
+        router.push('/home');
     }
 
-
-    const handleFacebookLogin = () => {
-        console.log("Facebook login");
-    }
 
     return (
         <div className="w-full">
-            <div className="flex flex-col items-center mb-2 rounded-2xl sm:rounded-3xl gap-2 sm:gap-3">
-                <div className="text-custom-content-primary text-center text-xl sm:text-heading-small">Login to your Account</div>
+            <div className="flex flex-col items-center my-4 rounded-2xl sm:rounded-3xl gap-2 sm:gap-3">
+                <div className="text-custom-content-primary text-center text-xl sm:text-heading-small">লগইন করুন</div>
                 <div className="text-custom-content-secondary text-sm sm:text-body-17 text-center">
-                    It's quick and easy
+                    খুব সহজেই লগইন করুন এবং প্রশ্নগুলো উত্তর দিন
                 </div>
             </div>
             <div className="flex flex-col gap-4 w-full max-w-full sm:max-w-md mx-auto">
 
-                <button
+                {/* <button
                     onClick={handleFacebookLogin}
                     className="flex items-center justify-center gap-2 w-full py-3 sm:py-3 px-2 sm:px-3 bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-50 transition-colors">
                     <Image src={FacebookIcon} alt="Facebook" width={20} height={20} className="w-5 h-5 sm:w-6 sm:h-6" />
                     <span className="text-gray-700 font-medium text-sm sm:text-base">ফেসবুক দিয়ে অ্যাকাউন্ট তৈরি করুন</span>
-                </button>
+                </button> */}
+                {/* Sign up with only username */}
+                <form onSubmit={handleLogin} className='flex flex-col gap-2 mb-4'>
+                    <div className="flex flex-col gap-2 mb-4">
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="username" className="text-custom-content-primary text-[14px] sm:text-[16px]">ইউজারনেম</label>
+                            <Input type="text" name="username" placeholder="ইউজারনেম" className='text-custom-content-primary py-6 dark:text-custom-content-white rounded-xl' />
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <label htmlFor="password" className="text-custom-content-primary text-[14px] sm:text-[16px]">পাসওয়ার্ড</label>
+                            <Input type="password" name="password" placeholder="পাসওয়ার্ড" className='text-custom-content-primary py-6 dark:text-custom-content-white rounded-xl' />
+                        </div>
+                    </div>
+                    <Button
+                        type="submit"
+                        variant="default"
+                        className="flex items-center justify-center gap-2 w-full py-3 sm:py-3 px-2 sm:px-3 rounded-lg shadow-sm transition-colors">
+                        লগইন করুন
+                    </Button>
+                </form>
+                <div className="flex flex-col items-center justify-center">
+                    <div className="w-full">
+                        <div className="w-full h-[1px] bg-custom-border-gray-light"></div>
+                        <p className="text-custom-content-secondary pt-3 text-sm sm:text-body-17 text-center">
+                            আপনি কোন অ্যাকাউন্ট নাই? তাহলে <br />
+                            <button onClick={onRegistration} className="text-custom-green">অ্যাকাউন্ট তৈরি করুন</button>
+                        </p>
+                    </div>
+                </div>
             </div>
-            {/* A bar between a word call "OR" */}
-            <div className="flex items-center gap-2 my-2">
-                <div className="flex-grow h-[1px] bg-custom-border-gray-light"></div>
-                <span className="text-custom-content-secondary text-body-17 px-2">OR</span>
-                <div className="flex-grow h-[1px] bg-custom-border-gray-light"></div>
-            </div>
-        </div>
+        </div >
     )
 }
 
