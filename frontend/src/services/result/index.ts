50a8ed6f0e352ba6fd15e8a9d73e461c81e4e7f6
Awalhossain <waqin21@gmail.com>
2024-10-26 15:06:01 +0600

    refactor: Update endpoints and error handling in result service

--- frontend/src/services/result/index.ts
+++ frontend/src/services/result/index.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { getAxiosError } from "@/lib/getAxiosError";
 import { AxiosResponse } from "axios";
-import { clientFetch } from "../client-fetch";
+import { toast } from "sonner";
 import { ENDPOINTS } from "../endpoints";
+import { serverFetch } from "../server-fetch";
 
 export const getResult = async ({userId, examId}: {userId: string, examId: string}): Promise<AxiosResponse<any>> => {
     try {
@@ -9,21 +11,27 @@ export const getResult = async ({userId, examId}: {userId: string, examId: strin
             userId,
             examId
         }).toString();
-        const response = await clientFetch.get(`${ENDPOINTS["RESULT"]}?${queryParams}`);
+        const response = await serverFetch.get(`${ENDPOINTS["RESULT"]}?${queryParams}`);
         return response;
     } catch (error: any) {
-        return Promise.reject(error);
+        const message = getAxiosError(error);
+        console.log(message, "message from here",error);
+        
+        // toast.error(`${message.message} ${message.status}`);
+        throw new Error(error.response?.data?.message || error.message || 'An error occurred while fetching the result');
     }
 }
 
-export const getResultLeaderboard = async ({examId, limit}: {examId: string, limit?: number}): Promise<AxiosResponse<any>> => {
+export const getResultLeaderboard = async ({examId, limit=100}: {examId: string, limit?: number}): Promise<AxiosResponse<any>> => {
     try {
         const url = `${ENDPOINTS["LEADERBOARD"]}/${examId}/leaderboard?limit=${limit}`;
         console.log(url, "url of leaderboard");
         
-        const response = await clientFetch.get(url);
+        const response = await serverFetch.get(url);
         return response;
     } catch (error: any) {
+        const message = getAxiosError(error);
+        toast.error(`${message.message} ${message.status}`);
         return Promise.reject(error);
     }
 }
