feb685d12983953505a0879a462aa26e88c1cb96
Awalhossain <waqin21@gmail.com>
2024-10-26 14:43:28 +0600

    refactor: Update ExamController and AuthGuard to handle public routes and extract JWT token from header

--- backend/src/questions/questions.controller.ts
+++ backend/src/questions/questions.controller.ts
@@ -5,9 +5,12 @@ import {
   Param,
   Post,
   Query,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from "@nestjs/common";
+import { SkipAuth } from "../auth/decorators/public.decorators";
+import { AuthGuard } from "../auth/guards/auth.guard";
 import { CreateQuestionDto } from "./dtos/createQuestio.dto";
 import { QuestionsService } from "./questions.service";
 
@@ -15,22 +18,25 @@ import { QuestionsService } from "./questions.service";
 export class QuestionsController {
   constructor(private questionsService: QuestionsService) {}
 
+  @UseGuards(AuthGuard)
   @Post("/create")
   @UsePipes(ValidationPipe)
   createQuestion(@Body() data: CreateQuestionDto) {
     return this.questionsService.createQuestion(data);
   }
 
+  @SkipAuth()
   @Get("/all")
   getAllQuestions() {
     return this.questionsService.getAllQuestions();
   }
-
+  @UseGuards(AuthGuard)
   @Get("/exam/:examId")
   getQuestionsByExamId(@Param("examId") examId: string) {
     return this.questionsService.getQuestionsByExamId(examId);
   }
 
+  @UseGuards(AuthGuard)
   @Get("/current")
   getCurrentQuestion(
     @Query("examSessionId") examSessionId: string,
