edcdc07341f5d8650f4952f1156821704c89350f
Awalhossain <waqin21@gmail.com>
2024-10-27 16:06:21 +0600

    removed unneccessary code

--- frontend/src/services/questions/index.ts
+++ frontend/src/services/questions/index.ts
@@ -1,21 +1,21 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { getAxiosError } from "@/lib/getAxiosError";
-import { AxiosResponse } from "axios";
-import { toast } from "sonner";
-import { ENDPOINTS } from "../endpoints";
-import { serverFetch } from "../server-fetch";
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import { getAxiosError } from "@/lib/getAxiosError";
+// import { AxiosResponse } from "axios";
+// import { toast } from "sonner";
+// import { ENDPOINTS } from "../endpoints";
+// import { serverFetch } from "../server-fetch";
 
 
 
 
 
-export const FinishExam = async (examSessionId: string): Promise<AxiosResponse<any>> => {
-    try {
-        const response = await serverFetch.post(`${ENDPOINTS["FINISH"]}`, { examSessionId });
-        return response;
-    } catch (error: any) {
-        const message = getAxiosError(error);
-            toast.error(`${message.message} ${message.status}`);
-            return Promise.reject(error);
-    }
-}
+// export const FinishExam = async (examSessionId: string): Promise<AxiosResponse<any>> => {
+//     try {
+//         const response = await serverFetch.post(`${ENDPOINTS["FINISH"]}`, { examSessionId });
+//         return response;
+//     } catch (error: any) {
+//         const message = getAxiosError(error);
+//             toast.error(`${message.message} ${message.status}`);
+//             return Promise.reject(error);
+//     }
+// }
