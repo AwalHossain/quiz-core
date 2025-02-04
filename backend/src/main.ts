5faea3fb8ce28135fc4198d6af446a424c05c9d3
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2025-02-04 14:13:46 +0000

    feat: add logging for DATABASE_URL verification in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: "*",
   });
-
   const options = new DocumentBuilder()
     .setTitle("MCQ API")
     .setDescription("The MCQ API description")
@@ -17,6 +17,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("api", app, document);
+
+    // Verify DATABASE_URL is loaded
+    console.log('DATABASE_URL exists:');
+  
 
   await app.listen(8000);
 }
