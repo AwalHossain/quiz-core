dcbfe012e2084f638843b9947af5106471f4672b
Awalhossain <waqin21@gmail.com>
2025-02-16 23:34:38 +0600

    refactor: clean up login form by removing console logs and dark mode classes

--- frontend/src/app/(group)/home/auth/LoginForm.tsx
+++ frontend/src/app/(group)/home/auth/LoginForm.tsx
@@ -26,12 +26,10 @@ const LoginForm: React.FC<LoginFormProps> = ({ onRegistration }) => {
         const formData = new FormData(e.target as HTMLFormElement);
         const username = formData.get('username') as string;
         const password = formData.get('password') as string;
-        console.log(username, "data from login form");
         try {
             const response = await SignIn(username, password);
             if (response && response.access_token) {
                 await setAuthToken('access_token', response.access_token);
-                console.log(response.access_token, "response from login form");
                 toast.success('লগইন সফলভাবে সম্পন্ন হয়েছে!');
                 window.location.href = "/home";
             } else {
@@ -60,22 +58,16 @@ const LoginForm: React.FC<LoginFormProps> = ({ onRegistration }) => {
             </div>
             <div className="flex flex-col gap-4 w-full max-w-full sm:max-w-md mx-auto">
 
-                {/* <button
-                    onClick={handleFacebookLogin}
-                    className="flex items-center justify-center gap-2 w-full py-3 sm:py-3 px-2 sm:px-3 bg-white border border-gray-300 rounded-lg shadow-sm hover:bg-gray-50 transition-colors">
-                    <Image src={FacebookIcon} alt="Facebook" width={20} height={20} className="w-5 h-5 sm:w-6 sm:h-6" />
-                    <span className="text-gray-700 font-medium text-sm sm:text-base">ফেসবুক দিয়ে অ্যাকাউন্ট তৈরি করুন</span>
-                </button> */}
-                {/* Sign up with only username */}
+ 
                 <form onSubmit={handleLogin} className='flex flex-col gap-2 mb-4'>
                     <div className="flex flex-col gap-2 mb-4">
                         <div className='flex flex-col gap-2'>
                             <label htmlFor="username" className="text-custom-content-primary text-[14px] sm:text-[16px]">ইউজারনেম</label>
-                            <Input type="text" name="username" placeholder="ইউজারনেম" className='text-custom-content-primary py-6 dark:text-custom-content-white rounded-xl' />
+                            <Input type="text" name="username" placeholder="ইউজারনেম" className='text-custom-content-primary py-6 rounded-xl' />
                         </div>
                         <div className='flex flex-col gap-2'>
                             <label htmlFor="password" className="text-custom-content-primary text-[14px] sm:text-[16px]">পাসওয়ার্ড</label>
-                            <Input type="password" name="password" placeholder="পাসওয়ার্ড" className='text-custom-content-primary py-6 dark:text-custom-content-white rounded-xl' />
+                            <Input type="password" name="password" placeholder="পাসওয়ার্ড" className='text-custom-content-primary py-6 rounded-xl' />
                         </div>
                     </div>
                     <Button
